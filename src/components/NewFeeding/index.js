94f2783e5b92be5281684dd7fc0b0d8ef4cfe688
Patrick Kennedy <pkennedy2@SF-US05643-MBP13.local>
2020-02-18 15:10:43 -0600

    finished all the routes and protected routes

---
+++ src/components/NewFeeding/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewFeeding() {
+    return(
+        <div>
+            <h1 className='page-header-title'>New Feeding</h1>
+        </div>
+    )
+}
---
+++ src/components/Profile/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Profile() {
+    return(
+        <div>
+            <h1 className='page-header-title'>Profile</h1>
+        </div>
+    )
+}
