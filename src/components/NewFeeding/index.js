563641f9cad20d1259e4ddf3db765ad5793b184f
Patrick Kennedy <pkennedy2@SF-US05643-MBP13.local>
2020-02-19 07:21:29 -0600

    finsihed the select dropdown with the state updating, need to now implement and case statment that will render different forms based on which selection is made

--- src/components/NewFeeding/index.js
+++ src/components/NewFeeding/index.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Dropdown } from 'semantic-ui-react'
 
 const options = [
-    { key: 'b', text: 'Breast Feeding', value: 'breast feeding', name: 'breast feeding' },
-    { key: 'p', text: 'Pumped Breast Milk', value: 'pumped breast milk', name: 'pumped' },
-    { key: 'f', text: 'Formula', value: 'formula', name: 'formula' },
-    { key: 'd', text: 'Doner Milk', value: 'doner milk', name: 'doner milk'}
+    { key: 'b', text: 'Breast Feeding', value: 'breast feeding' },
+    { key: 'p', text: 'Pumped Breast Milk', value: 'pumped breast milk' },
+    { key: 'f', text: 'Formula', value: 'formula' },
+    { key: 'd', text: 'Doner Milk', value: 'doner milk' }
   ]
 
 export default function NewFeeding() {
+    const [feedingType, setFeedingType] = useState(null)
+    
     return(
         <div className='page-content-container'>
             <h1 className='page-header-title'>New Feeding</h1>
             <Form>
-                <Form.Select onChange={(e) => console.log(e.target.getAttribute('name'))} label='Feeding Type' options={options} placeholder='Feeding Type' />
+                <Form.Select 
+                    onChange={(e) => setFeedingType(e.target.textContent)} 
+                    fluid 
+                    selection 
+                    label='Feeding Type' 
+                    options={options} 
+                    placeholder='Feeding Type' 
+                />
                 <Form.Group>
                 <Form.Input label='First name' placeholder='First Name' width={6} />
                 <Form.Input label='Middle Name' placeholder='Middle Name' width={4} />
